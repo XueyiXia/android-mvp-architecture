0ab7d118d0b7131a619f703775a0a3c74df20782
Bony <xueyi.xia@cn.aastocks.com>
2022-12-09 16:52:41 +0800

    remove old code

--- framework_mvp/src/main/java/com/framework/mvp/mode/BaseModel.kt
+++ framework_mvp/src/main/java/com/framework/mvp/mode/BaseModel.kt
@@ -1,17 +1,18 @@
 package com.framework.mvp.mode
 
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.OnLifecycleEvent
+import android.util.Log
+import androidx.lifecycle.*
 import com.framework.mvp.interfac.IModel
+import com.framework.mvp.presenter.BasePresenter
 import io.reactivex.disposables.Disposable
 
-abstract class BaseModel : IModel, LifecycleObserver {
+abstract class BaseModel : IModel, DefaultLifecycleObserver {
 
+    companion object{
+        var TAG:String="BaseModel"
+    }
+    
     override fun addDisposable(disposable: Disposable) {
-
-
 
     }
 
@@ -24,8 +25,36 @@ abstract class BaseModel : IModel, LifecycleObserver {
      * 清除订阅关系
      */
     private fun disposable() {}
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun onDestroy(lifecycleOwner: LifecycleOwner?) {
-        lifecycleOwner?.lifecycle?.removeObserver(this)
+
+
+    override fun onCreate(owner: LifecycleOwner) {
+        Log.e(TAG, "LifecycleObserver-> onCreate${owner}")
+        super.onCreate(owner)
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        Log.e(TAG, "LifecycleObserver-> onStart :${owner}")
+        super.onStart(owner)
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        Log.e(TAG, "LifecycleObserver-> onResume :${owner}")
+        super.onResume(owner)
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        Log.e(TAG, "LifecycleObserver->onPause :${owner}")
+        super.onPause(owner)
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        Log.e(TAG, "LifecycleObserver-> onStop :${owner}")
+        super.onStop(owner)
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        Log.e(TAG, "LifecycleObserver--> onDestroy :${owner.lifecycle}")
+        super.onDestroy(owner)
+        owner.lifecycle.removeObserver(this)
     }
 }
